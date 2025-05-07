ec8418710f8ef31f64948ab530cd6b4af1cab919
qeratos <nikita@MacBook-Pro-Nikita.local>
2025-05-07 22:21:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+gcc .\peb.obj -o peb.exe -nostartfiles
+nasm.exe -f win64 .\peb\peb.asm -o peb.obj
---
+++ peb.asm
@@ -0,0 +1,108 @@
+section .data
+    module_array     times 256 dq 0   
+    name_array       times 256 dq 0   
+    module_count     dq 0             
+    buffer           times 128 dq 0
+    lib_name         dw "K","E","R","N","E","L","3","2",".","D","L","L",0,0,0
+    ; lib_size         equ $ - lib_name-1
+    dll_addr         dq 0
+
+section .text
+    global _start
+
+
+
+_start:
+    ; Получаем PEB
+    mov rax, [gs:0x60]           ; PEB
+    mov rax, [rax + 0x18]        ; PEB_LDR_DATA
+    mov rsi, [rax + 0x10]        ; InLoadOrderModuleList | _LDR_DATA_TABLE_ENTRY
+    
+    xor rdi, rdi             
+    lea r15, [rel module_array]    
+    lea r14, [rel name_array]       
+
+    modules_loop:
+        mov rbx, [rsi + 0x30]        ; 0x30 DllBase
+
+        cmp rbx, 0
+        je done
+
+        mov [rel dll_addr], rbx
+        mov rcx, qword[rsi + 0x60]  
+        push rsi 
+        push rax
+
+        mov rsi, lib_name
+        mov rdi, rcx
+        call compare_str
+
+        cmp rax, 1
+        je pass
+
+        call dll_parce
+
+        pass:
+        pop rax
+        pop rsi
+
+        mov rsi, [rsi]         ; [+0x000] Flink   
+        cmp rsi, [rax + 0x20]
+
+        je done             
+        loop modules_loop
+
+    done:
+        mov [rel module_count], rdi
+        leave
+        ret
+
+
+;Вход: dll_addr = адрес dll
+dll_parce:
+    push rbp
+    mov rbp, rsp
+
+    mov rax, [rel dll_addr]     ; DLL PTR    
+
+    mov ebx, dword[rax + 0x180] ; EXPORT RVA
+    add rax, rbx                ; EXPORT TABLE HEADER
+
+[]
+    leave
+    ret
+
+
+
+;Вход: rsi = строка1, rdi = строка2
+compare_str:
+    push rbp
+    mov rbp, rsp
+
+    xor rax, rax
+    xor rbx, rbx
+
+    .compare:
+        mov ax, [rsi]       ; Загружаем символ (2 байта)
+        mov bx, [rdi]
+
+        cmp ax, bx
+        jne .not_equal      ; Выход если не равны
+
+        test ax, ax         ; Проверка на нуль-терминатор
+        jz .equal           ; Если 0 - строки равны
+
+        add rsi, 2          ; Следующий символ
+        add rdi, 2
+
+        jmp .compare
+
+    .equal:
+        xor eax, eax 
+        leave
+        ret
+    
+    .not_equal:
+        mov eax, 1  
+        leave
+        ret
